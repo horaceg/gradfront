fce2fff3371c5ebdb868fd793a99ad188b63882d
Horace Guy <horace.guy.k@gmail.com>
2021-04-18 21:17:18 +0200

    format

--- src/lib/Range.svelte
+++ src/lib/Range.svelte
@@ -1,19 +1,19 @@
 <script>
-	export let value;
-	export let name;
-	export let mini;
-	export let maxi;
-	export let step;
+  export let value;
+  export let name;
+  export let mini;
+  export let maxi;
+  export let step;
 </script>
 
 <div>
-	<label for={name}>{name}</label>
-	<input id={name} type="number" min={mini} max={maxi} {step} bind:value />
-	<input id={name} type="range" min={mini} max={maxi} {step} bind:value />
+  <label for={name}>{name}</label>
+  <input id={name} type="number" min={mini} max={maxi} {step} bind:value />
+  <input id={name} type="range" min={mini} max={maxi} {step} bind:value />
 </div>
 
 <style>
-	input[type='number'] {
-		width: 3.1em;
-	}
+  input[type="number"] {
+    width: 3.1em;
+  }
 </style>
